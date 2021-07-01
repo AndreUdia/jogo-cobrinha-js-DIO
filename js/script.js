af2f4d1d836c8fa8c56da916118a49f3ef3b4cfd
Cabomaldade <soutom@yahoo.com.br>
2021-07-01 02:03:11 -0300

    Finalizado layout e regras - funcionalidade de encerramento

--- js/script.js
+++ js/script.js
@@ -44,6 +44,13 @@ function iniciarJogo() {
   if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
   if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+  for(let i=1; i < snake.length; i++) {
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(game);
+      alert('Você Perdeu! :( Tente Novamente com F5');
+    }
+  }
+
   criarBG();
   criarCobrinha();
   drawFood();
@@ -56,7 +63,14 @@ function iniciarJogo() {
   if (direction == 'up') snakeY -= box;
   if (direction == 'down') snakeY += box;
 
-  snake.pop();
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
+
+  
 
   let newHead = {
     x: snakeX,
